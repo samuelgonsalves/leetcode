2749aa78d7b89192e3c4285f9b0d0b15ef3982dd
sdg123 <sgonsal@ncsu.edu>
2020-09-03 10:53:18 -0400

    Cleanup and comments

--- Arrays/Arrays/TwoSumProblem.cs
+++ Arrays/Arrays/TwoSumProblem.cs
@@ -5,23 +5,22 @@ namespace Arrays
 {
     public static class TwoSumProblem
     {
-            public static int[] TwoSum(int[] nums, int target)
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
             {
-                var dict = new Dictionary<int, int>();
+                var key = target - nums[i];
 
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    var key = target - nums[i];
+                if (dict.ContainsKey(key))
+                    return new int[] { i, dict[key] };
 
-                    if (dict.ContainsKey(key))
-                        return new int[] { i, dict[key] };
-
-                    if (!dict.ContainsKey(nums[i]))
-                        dict.Add(nums[i], i);
-                }
-
-                throw new Exception("Exactly one solution does not exist");
+                if (!dict.ContainsKey(nums[i])) // In case there are duplicates in nums[]
+                    dict.Add(nums[i], i); // Saving {value, index} for O(1) retrieval
             }
-        }
 
+            throw new Exception("Exactly one solution does not exist");
+        }
+    }
 }
