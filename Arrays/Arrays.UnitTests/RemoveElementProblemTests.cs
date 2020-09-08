ddc38d4444b4d298fd665ecad0e7ccc2f6cae32e
sdg123 <sgonsal@ncsu.edu>
2020-09-08 11:57:13 -0400

    Solved RemoveElement

---
+++ Arrays/Arrays.UnitTests/RemoveElementProblemTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Arrays.UnitTests
+{
+    public class RemoveElementProblemTests
+    {
+        [Fact]
+        public void RemoveElementTest()
+        {
+            var lengthOfListWithRemovedElement = RemoveElementProblem.RemoveElement(new int[] { 1, 2, 2, 3, 5 }, 2);
+
+            lengthOfListWithRemovedElement.Should().Be(3);
+
+        }
+    }
+}
---
+++ Arrays/Arrays/RemoveElementProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public static class RemoveElementProblem
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0] != val ? 1 : 0;
+            }
+
+            int i = 0;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] != val)
+                {
+                    i++;
+                }
+                else if (nums[j] != val)
+                {
+                    var temp = nums[i];
+                    nums[i++] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+            return i;
+        }
+    }
+}
