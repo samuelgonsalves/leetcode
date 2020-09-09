4052d4452b865adfe879f607bb1e34d467aca632
sdg123 <sgonsal@ncsu.edu>
2020-09-09 16:09:01 -0400

    Solved merge sorted arrays

---
+++ Arrays/Arrays.UnitTests/MergeSortedArraysTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Arrays.UnitTests
+{
+    public class MergeSortedArraysTests
+    {
+        [Fact]
+        public void MergeTest()
+        {
+            var nums1 = new int[] { 1, 4, 6, 7, 9, 0, 0, 0, 0 };
+            var nums2 = new int[] { 2, 3, 8, 10 };
+
+            MergeSortedArrays.Merge(nums1, nums1.Length - nums2.Length, nums2, nums2.Length).Should().BeEquivalentTo(new int[] { 1, 2, 3, 4, 6, 7, 8, 9, 10 });
+        }
+    }
+}
---
+++ Arrays/Arrays/MergeSortedArrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public static class MergeSortedArrays
+    {
+        public static int[] Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int t = m + n - 1, i = m - 1, j = n - 1; t >= 0; t--)
+            {
+                if (i < 0)
+                {
+                    nums1[t] = nums2[j--];
+                }
+                else if (j < 0)
+                {
+                    nums1[t] = nums1[i--];
+                }
+                else
+                {
+                    if (nums1[i] > nums2[j])
+                    {
+                        nums1[t] = nums1[i--];
+                    }
+                    else
+                    {
+                        nums1[t] = nums2[j--];
+                    }
+                }
+            }
+
+            return nums1;
+        }
+
+    }
+}
