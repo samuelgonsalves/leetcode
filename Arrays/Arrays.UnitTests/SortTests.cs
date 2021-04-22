9a60d33d7b8dfd30d7588eac2a988cc6a4f6fe1c
sdg123 <sgonsal@ncsu.edu>
2021-04-22 11:08:01 -0400

    Add merge sort

---
+++ Arrays/Arrays.UnitTests/SortTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Arrays.UnitTests
+{
+    public class SortTests
+    {
+        [Fact]
+        public void MergeSortTest()
+        {
+            Sort.MergeSort(new int[] { 1, 3, 7, 8, 2 }).Should().BeEquivalentTo(new int[] { 1, 2, 3, 7, 8 });
+        }
+    }
+}
